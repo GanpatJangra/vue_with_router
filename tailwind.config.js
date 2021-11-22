f7da8483021c64eaf60bc3f9ce5ddfdffc738206
Ganpat Jangra <69577968+GanpatJangra@users.noreply.github.com>
2021-11-22 12:57:13 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
